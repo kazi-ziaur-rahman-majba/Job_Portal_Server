539b9b96f7c23cf37919f0f1b335d54a1be40680
kazi-ziaur-rahman-majba <majba9399@gmail.com>
2026-02-08 20:55:12 +0600

    create single page apu

--- index.js
+++ index.js
@@ -35,6 +35,14 @@ async function run() {
             console.log(result);
         })
 
+        // Single Job API
+        app.get('/jobs/:id', async (req, res) =>{
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await jobsCollection.findOne(query);
+            res.send(result);
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
